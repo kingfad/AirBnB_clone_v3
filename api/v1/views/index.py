4570b11a352388d4c6a106932a1d35dd547e7890
Fawaz Oduola <oduolafawaz@gmail.com>
2022-10-25 19:16:12 +0100

    task 4 done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,16 @@ This module contains route
 to check status of API
 """
 
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
+from models import state
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.user import User
+from models.place import Place
 
 @app_views.route('/status', methods=["GET"])
 def status():
@@ -13,3 +21,18 @@ def status():
         API status
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=["GET"])
+def stats():
+    """
+        API stats
+    """
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(stats)
